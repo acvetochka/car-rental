e79090eb3aee12dd2dd659b7619fad71998bf974
Alona Kuznietsova <acvetochka@gmail.com>
2023-10-17 21:14:13 +0200

    initial project

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    ></div>
+  );
+};
