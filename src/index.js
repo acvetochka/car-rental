bced054a6d337d7d7fc4bbc7f88f46a221616f5a
Alona Kuznietsova <acvetochka@gmail.com>
2023-10-18 23:23:32 +0200

    add variables and fonts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
+import 'modern-normalize/modern-normalize.css';
+import './styles/variables.css';
+import './styles/fonts.css';
+import './index.css';
 // import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
