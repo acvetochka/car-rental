b21a54ff25d92f1aed3b63f57ef0d9b870045bba
acvetochka <116402791+acvetochka@users.noreply.github.com>
2023-10-19 14:58:02 +0000

    Deploying to gh-pages from @ acvetochka/car-rental@9b6c367d2aa0a9e977bd63c17f5e8a0a9941539a 🚀

--- static/js/395.23572305.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcar_rental=self.webpackChunkcar_rental||[]).push([[395],{7395:function(e,n,a){a.r(n);n.default=function(){}}}]);
-//# sourceMappingURL=395.23572305.chunk.js.map
